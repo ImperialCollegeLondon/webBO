025a373414152704de980c84277d9b2b17780c33
austin-mroz <austin.mroz@gmail.com>
2024-05-21 22:24:28 +0100

    Set debug to True

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ os.environ["BAYBE_TELEMETRY_ENABLED"] = "false"
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8050, debug=False)
+    app.run(host="localhost", port=8050, debug=True)
