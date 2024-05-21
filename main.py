8de17b3dab2a11460679941acf88aa6173b60583
austin-mroz <austin.mroz@gmail.com>
2024-05-21 16:08:55 +0100

    update port

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ os.environ["BAYBE_TELEMETRY_ENABLED"] = "false"
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="localhost", port=8081, debug=True)
+    app.run(host="localhost", port=8050, debug=True)
