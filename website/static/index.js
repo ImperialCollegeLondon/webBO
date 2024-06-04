f79c8ee29fa9051ebf4d1e2031d4350feef550ee
austin-mroz <austin.mroz@gmail.com>
2024-06-04 14:28:46 +0100

    Add URL_PREFIX to javascript functions
    Several Web-BO features are implemented using javascript. The `fetch`
    calls require the URL_PREFIX to work within the virtual machine.

--- website/static/index.js
+++ website/static/index.js
@@ -1,23 +1,23 @@
 function deleteDataset(nodeId) {
-    fetch('/delete-dataset', { method: 'POST', body: JSON.stringify({ noteId: noteId }) }).then((_res) => {
+    fetch(process.env.URL_PREFIX + '/delete-dataset', { method: 'POST', body: JSON.stringify({ noteId: noteId }) }).then((_res) => {
         WebTransportBidirectionalStream.location.href = "/";
     });
 }
 
 function deleteExperiment(nodeId) {
-    fetch('/delete-experiment', { method: 'POST', body: JSON.stringify({ noteId: noteId }) }).then((_res) => {
+    fetch(process.env.URL_PREFIX + '/delete-experiment', { method: 'POST', body: JSON.stringify({ noteId: noteId }) }).then((_res) => {
         WebTransportBidirectionalStream.location.href = "/";
     });
 }
 
 function addSampleDataset() {
-    fetch('/add-sample-dataset', { method: 'POST' }).then((_res) => {
+    fetch(process.env.URL_PREFIX + '/add-sample-dataset', { method: 'POST' }).then((_res) => {
         WebTransportBidirectionalStream.location.href = "/";
     });
 }
 
 function addParameterElement(nodeId) {
-    fetch('/add-parameter-element', { method: 'POST', body: JSON.stringify({ noteId: noteId }) }).then((_res) => {
+    fetch(process.env.URL_PREFIX + '/add-parameter-element', { method: 'POST', body: JSON.stringify({ noteId: noteId }) }).then((_res) => {
         WebTransportBidirectionalStream.location.href = "/";
     });
 }
